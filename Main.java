f921664bf323be242861641b3d174b6bfa704775
Godwin <godwinmail6@gmail.com>
2023-10-11 22:00:24 +0200

    Created orders liat, instantiated the object.

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+import entities.Customer;
 import entities.Order;
 import entities.Product;
 import org.w3c.dom.ls.LSOutput;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,11 @@ public class Main {
     public static void main(String[] args) {
         Random randomNumber = new Random();
 
+        Customer customer = new Customer(randomNumber.nextInt(0, 30), "Kade", 4);
+        Customer customer2 = new Customer(randomNumber.nextInt(0, 30), "Weeknd", 1);
+        Customer customer3 = new Customer(randomNumber.nextInt(0, 30), "Warriior", 3);
+        Customer customer4 = new Customer(randomNumber.nextInt(0, 30), "Lion", 5);
+        Customer customer5 = new Customer(randomNumber.nextInt(0, 30), "Cat", 2);
 
         Product product1 = new Product(randomNumber.nextInt(0, 100), "Shakespeare", "Books",
                 randomNumber.nextInt(50, 200));
@@ -25,13 +31,13 @@ public class Main {
                 randomNumber.nextInt(50, 200));
         Product product5 = new Product(randomNumber.nextInt(0, 100), "Parent", "Baby",
                 randomNumber.nextInt(50, 200));
-        Product product6 = new Product(randomNumber.nextInt(0, 100), "Hormones", "Books",
+        Product product6 = new Product(randomNumber.nextInt(0, 100), "Hormones", "Boys",
                 randomNumber.nextInt(50, 200));
         Product product7 = new Product(randomNumber.nextInt(0, 100), "Christian", "Books",
                 randomNumber.nextInt(50, 200));
         Product product8 = new Product(randomNumber.nextInt(0, 100), "Tech", "Babies",
                 randomNumber.nextInt(50, 200));
-        Product product9 = new Product(randomNumber.nextInt(0, 100), "AI", "Books",
+        Product product9 = new Product(randomNumber.nextInt(0, 100), "AI", "Boys",
                 randomNumber.nextInt(50, 200));
         Product product10 = new Product(randomNumber.nextInt(0, 100), "Job", "Books",
                 randomNumber.nextInt(50, 200));
@@ -40,10 +46,47 @@ public class Main {
                 product7, product8, product9, product10};
 
 
-        List<Product> eachProduct = new ArrayList<>(Arrays.asList(productsArr));
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        LocalDate date = LocalDate.of(2023, Month.OCTOBER, 10);
+        System.out.println(date);
 
-        List<Product> onlyBooks = eachProduct.stream().filter(i -> i.getCategory().equals("Books") && i.getPrice() > 100).toList();
+        List<Product> eachProduct = new ArrayList<>(Arrays.asList(productsArr));
+        List<Product> onlyBooks = eachProduct.stream().filter(book -> book.getCategory().equals("Books") && book.getPrice() > 100).toList();
         onlyBooks.forEach(System.out::println);
+
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<2<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        //creating orders
+        LocalDate deliveryDate = LocalDate.of(2023, Month.OCTOBER, 19);
+        Order order = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer3);
+        Order order2 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer4);
+        Order order3 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer2);
+        Order order4 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer2);
+        Order order5 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer2);
+        Order order6 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer2);
+        Order order7 = new Order(randomNumber.nextInt(0, 100), "On Sale", date, deliveryDate, eachProduct, customer2);
+
+        List<Order> orders = new ArrayList<>();
+        orders.add(order);
+        orders.add(order2);
+        orders.add(order3);
+        orders.add(order4);
+        orders.add(order5);
+        orders.add(order6);
+        orders.add(order7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
